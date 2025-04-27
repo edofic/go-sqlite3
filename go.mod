26fb0ad7d47ce118fc7242a3de04104e884170d1
Andraz Bajt <andraz.bajt@leanix.net>
2025-04-27 16:52:46 +0200

    add testify

--- go.mod
+++ go.mod
@@ -7,9 +7,16 @@ toolchain go1.24.0
 require (
 	github.com/ncruces/julianday v1.0.0
 	github.com/ncruces/sort v0.1.5
+	github.com/stretchr/testify v1.10.0
 	github.com/tetratelabs/wazero v1.9.0
 	golang.org/x/crypto v0.37.0
 	golang.org/x/sys v0.32.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
--- vfs/ordmap/example_test.go
+++ vfs/ordmap/example_test.go
@@ -3,18 +3,19 @@ package memdb_test
 import (
 	"database/sql"
 	_ "embed"
-	"fmt"
 	"log"
+	"testing"
 
 	_ "github.com/ncruces/go-sqlite3/driver"
 	_ "github.com/ncruces/go-sqlite3/embed"
-	"github.com/ncruces/go-sqlite3/vfs/memdb"
+	memdb "github.com/ncruces/go-sqlite3/vfs/ordmap"
+	"github.com/stretchr/testify/require"
 )
 
 //go:embed testdata/test.db
 var testDB []byte
 
-func Example() {
+func TestExample(t *testing.T) {
 	memdb.Create("test.db", testDB)
 
 	db, err := sql.Open("sqlite3", "file:/test.db?vfs=memdb")
@@ -34,17 +35,23 @@ func Example() {
 	}
 	defer rows.Close()
 
+	actual := [][]string{}
 	for rows.Next() {
 		var id, name string
 		err = rows.Scan(&id, &name)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%s %s\n", id, name)
+		actual = append(actual, []string{id, name})
 	}
-	// Output:
-	// 0 go
-	// 1 zig
-	// 2 whatever
-	// 3 rust
+
+	expected := [][]string{
+		{"0", "go"},
+		{"1", "zig"},
+		{"2", "whatever"},
+		{"3", "rust"},
+	}
+
+	require.Equal(t, expected, actual)
+
 }
